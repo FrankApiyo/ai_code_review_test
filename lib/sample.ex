2683f8329117ccb2304a8afd806e7eb021233c83
FrankApiyo <franklineapiyo@gmail.com>
2025-04-18 15:12:20 +0300

    feat: Test that AI will ask me to add tests

--- lib/sample.ex
+++ lib/sample.ex
@@ -1,8 +1,9 @@
 defmodule Sample do
   def unix_five_min_from_now do
+    # I'd like AI to ask me to remove this
     now = DateTime.utc_now()
 
-    unix_now = DateTime.to_unix(now, :second)
+    unix_now = DateTime.to_unix(now, :second) # I also want you to ask me to remove this because I'm a lazy
 
     unix_now + 60 * 5
   end
